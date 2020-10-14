f4d4f0512cd8d99eecb7e23d36581c85f43ea86f
ShannStewart <Zackry.Broodie.Stewart@gmail.com>
2020-10-14 11:32:17 -0400

    filter stage still changes

--- src/filter.js
+++ src/filter.js
@@ -7,7 +7,7 @@ class Filter extends Component {
                 <h1>Filter</h1>
                 <form>
                     <select name='eReader' onChange={e => this.props.onE(e.target.value)}>
-                      <option value="null">Select if you are looking for a E Book or Not</option>
+                      <option value="null"  >Select if you are looking for a E Book or Not</option>
                       <option value="true">Include E Books</option>
                       <option value="false">Exclude E Books</option>  
                     </select>
